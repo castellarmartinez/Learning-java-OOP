df08a7d7fc4947d0106a73529133a4fa3e46e255
David Castellar Martínez <castellardav@gmail.com>
2022-04-01 08:42:21 -0500

    Find the probability of win in the grame of Craps

--- src/chapter07/exercises/UsingEnhancedForStatement.java
+++
@@ -1,9 +0,0 @@
-package chapter07.exercises;
-
-public class UsingEnhancedForStatement {
-    public static void main(String[] args) {
-        for (String number: args) {
-            System.out.printf("%5d", Integer.parseInt(number));
-        }
-    }
-}
---
+++ src/chapter07/exercises/ex7_16_Using_Enhanced_For_Stament/UsingEnhancedForStatement.java
@@ -0,0 +1,9 @@
+package chapter07.exercises.ex7_16_Using_Enhanced_For_Stament;
+
+public class UsingEnhancedForStatement {
+   public static void main(String[] args) {
+      for (String number : args) {
+         System.out.printf("%5d", Integer.parseInt(number));
+      }
+   }
+}
