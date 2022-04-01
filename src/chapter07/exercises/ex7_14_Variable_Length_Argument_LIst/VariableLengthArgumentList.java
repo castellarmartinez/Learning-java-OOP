10ec953f46937bcf9c36d93b33771676630c791a
David Castellar Martínez <castellardav@gmail.com>
2022-04-01 08:33:53 -0500

    Finish command-line exercises using arrays

---
+++ src/chapter07/exercises/ex7_14_Variable_Length_Argument_LIst/VariableLengthArgumentList.java
@@ -0,0 +1,14 @@
+package chapter07.exercises.ex7_14_Variable_Length_Argument_LIst;
+
+public class VariableLengthArgumentList {
+   public static void main(String[] args) {
+      double sum = 0;
+
+      for (int i = 0; i < args.length; i++) {
+         sum += Integer.parseInt(args[i]);
+      }
+
+      System.out.printf("The average of the integers entered as argument list:"
+              + " %.1f", (sum / args.length));
+   }
+}
---
+++ src/chapter07/exercises/ex7_15_Command_Line_Arguments/CommandLineArguments.java
@@ -0,0 +1,24 @@
+package chapter07.exercises.ex7_15_Command_Line_Arguments;
+
+public class CommandLineArguments {
+   public static void main(String[] args) {
+      int max = Integer.parseInt(args[0]);
+      int min = Integer.parseInt(args[0]);
+      int average = 0;
+
+      for (int i = 0; i < args.length; i++) {
+         int current = Integer.parseInt(args[i]);
+
+         if (current > max) {
+            max = current;
+         }
+         if (current < min) {
+            min = current;
+         }
+      }
+
+      average = max - min;
+      System.out.printf("The average between the maximum and minimum is: %d",
+              average);
+   }
+}
