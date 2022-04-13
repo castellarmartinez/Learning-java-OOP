0d69c313a85bd09e61d1be2864afd9a422128c3f
David Castellar Martínez <castellardav@gmail.com>
2022-04-13 11:42:42 -0500

    Finishes Car class for exercise 3.13

---
+++ src/chapter03/exercises/ex3_13_Car_Class/CarApplication.java
@@ -0,0 +1,26 @@
+package chapter03.exercises.ex3_13_Car_Class;
+
+public class CarApplication {
+   public static void main(String[] args) {
+      Car car1 = new Car("Optra", "2008", 12_000);
+      Car car2 = new Car("Corolla", "2005", 15_000);
+
+      System.out.printf("The price of the %s is %.2f%n", car1.getModel(),
+              car1.getPrice());
+      System.out.printf("The price of the %s is %.2f%n", car2.getModel(),
+              car2.getPrice());
+
+      System.out.printf("Applying a 5%% discount on the price of the %s%n",
+              car1.getModel());
+      car1.setPrice(car1.getPrice() * 0.95);
+
+      System.out.printf("Applying a 7%% discount on the price of the %s%n",
+              car2.getModel());
+      car2.setPrice(car2.getPrice() * 0.93);
+
+      System.out.printf("The new price of the %s is %.2f%n", car1.getModel(),
+              car1.getPrice());
+      System.out.printf("The new price of the %s is %.2f%n", car2.getModel(),
+              car2.getPrice());
+   }
+}
---
+++ src/chapter03/exercises/ex3_13_Car_Class/readme.md
@@ -0,0 +1,8 @@
+### Car Class 
+
+Create a class called _Car_ that includes three instance variables—a model (type
+_String_), a year (type _String_), and a price (_double_). Provide a constructor that initializes the three
+instance variables. Provide a **_set_** and a get method for each instance variable. If the price is not positive,
+do not set its value. Write a test application named _CarApplication_ that demonstrates class _Car_’s capabilities.
+Create two _Car_ objects and display each object’s price. Then apply a 5% discount on the
+price of the first _car_ and a 7% discount on the price of the second. Display each _Car_’s price again.
