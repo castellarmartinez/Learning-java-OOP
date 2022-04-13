59660729f44a6760c641577f22bf17184a08c915
David Castellar Martínez <castellardav@gmail.com>
2022-04-12 23:00:51 -0500

    Creates a class called PetrolPurchase to represent information about a petrol

---
+++ src/chapter03/exercises/ex3_12_Class_PetrolPurchase/PetrolPurchase.java
@@ -0,0 +1,62 @@
+package chapter03.exercises.ex3_12_Class_PetrolPurchase;
+
+public class PetrolPurchase {
+   private String location;
+   private String typeOfPetrol;
+   private int quantity;
+   private double pricePerLitter;
+   private double discountPercentage;
+
+   public PetrolPurchase(String location, String typeOfPetrol, int quantity,
+                         double pricePerLitter, double discountPercentage) {
+      this.location = location;
+      this.typeOfPetrol = typeOfPetrol;
+      this.quantity = quantity;
+      this.pricePerLitter = pricePerLitter;
+      this.discountPercentage = discountPercentage;
+   }
+
+   public String getLocation() {
+      return location;
+   }
+
+   public void setLocation(String location) {
+      this.location = location;
+   }
+
+   public String getTypeOfPetrol() {
+      return typeOfPetrol;
+   }
+
+   public void setTypeOfPetrol(String typeOfPetrol) {
+      this.typeOfPetrol = typeOfPetrol;
+   }
+
+   public int getQuantity() {
+      return quantity;
+   }
+
+   public void setQuantity(int quantity) {
+      this.quantity = quantity;
+   }
+
+   public double getPricePerLitter() {
+      return pricePerLitter;
+   }
+
+   public void setPricePerLitter(double pricePerLitter) {
+      this.pricePerLitter = pricePerLitter;
+   }
+
+   public double getDiscountPercentage() {
+      return discountPercentage;
+   }
+
+   public void setDiscountPercentage(double discountPercentage) {
+      this.discountPercentage = discountPercentage;
+   }
+
+   public double getPurchaseAmount() {
+      return this.quantity * this.pricePerLitter * (1 - discountPercentage);
+   }
+}
---
+++ src/chapter03/exercises/ex3_12_Class_PetrolPurchase/Readme.md
@@ -0,0 +1,10 @@
+### Class _PetrolPurchase_
+
+Create a class called **_PetrolPurchase_** to represent information
+about the petrol you purchase. The class should include five pieces of information in the form of
+instance variables—the station’s location (type _String_), the type of petrol (type _String_), the quantity
+(type _int_) of the purchase in liters, the price per liter (_double_), and the percentage discount
+(_double_). Your class should have a constructor that initializes the five instance variables. Provide a
+**set** and a **get** method for each instance variable. In addition, provide a method named **_getPurchaseAmount_** that calculates the net purchase amount (i.e., multiplies the quantity by the price per liter)
+minus the discount, then returns the net amount you had to pay as a _double_ value. Write an application
+class named **_Petrol_** that demonstrates the capabilities of class **_PetrolPurchase_**.
