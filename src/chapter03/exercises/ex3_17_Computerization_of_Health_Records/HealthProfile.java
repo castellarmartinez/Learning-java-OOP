4817a414e58353971f6b577941354abbd25ece96
David Castellar Martínez <castellardav@gmail.com>
2022-04-17 15:25:12 -0500

    Finishes Computerization of Health Records

--- src/chapter03/exercises/ex3_17_Computerization_of_Health_Records/HealthProfile.java
+++ src/chapter03/exercises/ex3_17_Computerization_of_Health_Records/HealthProfile.java
@@ -1,18 +1,20 @@
 package chapter03.exercises.ex3_17_Computerization_of_Health_Records;
 
+import java.util.Date;
+
 public class HealthProfile {
    private String firstName;
    private String lastName;
    private String gender;
-   private final String dayOfBirth;
-   private final String monthOfBirth;
-   private final String yearOfBirth;
-   private String height;
-   private String weight;
+   private final int dayOfBirth;
+   private final int monthOfBirth;
+   private final int yearOfBirth;
+   private int height;
+   private int weight;
 
    public HealthProfile(String firstName, String lastName, String gender,
-                        String dayOfBirth, String monthOfBirth,
-                        String yearOfBirth, String height, String weight) {
+                        int dayOfBirth, int monthOfBirth, int yearOfBirth,
+                        int height, int weight) {
 
       this.firstName = firstName;
       this.lastName = lastName;
@@ -48,31 +50,68 @@ public class HealthProfile {
       this.gender = gender;
    }
 
-   public String getDayOfBirth() {
+   public int getDayOfBirth() {
       return dayOfBirth;
    }
 
-   public String getMonthOfBirth() {
+   public int getMonthOfBirth() {
       return monthOfBirth;
    }
 
-   public String getYearOfBirth() {
+   public int getYearOfBirth() {
       return yearOfBirth;
    }
 
-   public String getHeight() {
+   public int getHeight() {
       return height;
    }
 
-   public void setHeight(String height) {
+   public void setHeight(int height) {
       this.height = height;
    }
 
-   public String getWeight() {
+   public int getWeight() {
       return weight;
    }
 
-   public void setWeight(String weight) {
+   public void setWeight(int weight) {
       this.weight = weight;
+   }
+
+   public String getBirthDate() {
+      return getDayOfBirth() + "/" + getMonthOfBirth() + "/" + getYearOfBirth();
+   }
+
+   public int getAge() {
+      Date today = new Date();
+
+      if ((today.getMonth() + 1) > this.monthOfBirth) {
+         return (today.getYear() + 1900) - this.yearOfBirth;
+      }
+
+      if (this.monthOfBirth == (today.getMonth() + 1)) {
+         if (today.getDate() >= this.dayOfBirth) {
+            return (today.getYear() + 1900) - this.yearOfBirth;
+         }
+      }
+
+      return (today.getYear() + 1900) - this.yearOfBirth - 1;
+   }
+
+   public int getMaximumHeartRate() {
+      return 220 - getAge();
+   }
+
+   public String getTargetHeartRate() {
+      return (int) (getMaximumHeartRate() * 0.5) + "-"
+              + (int) (getMaximumHeartRate() * 0.85);
+   }
+
+   public String getBMI() {
+      int BMI = weight * 10_000 / (height * height);
+
+      return "The body mass index is " + BMI + "\nUnderweight = <18.5"
+              + "\nNormal weight = 18.5–24.9\nOverweight = 25–29.9"
+              + "\nObesity = BMI of 30 or greater";
    }
 }
