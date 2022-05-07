17f4971f185a4e825de7275800396a84bb980be2
David Castellar Martínez <castellardav@gmail.com>
2022-05-06 21:18:03 -0500

    Completes sportsRecommender method

---
+++ src/chapter06/exercises/ex6_28/SportRecommender.java
@@ -0,0 +1,25 @@
+package chapter06.exercises.ex6_28;
+
+import java.util.Scanner;
+
+public class SportRecommender {
+   public static void main(String[] args) {
+      Scanner input = new Scanner(System.in);
+      double currentTemperature;
+
+      System.out.print("Input the current temperature: ");
+      currentTemperature = input.nextDouble();
+
+      System.out.println(sportsRecommender(currentTemperature));
+   }
+
+   private static String sportsRecommender(double temperature) {
+      if (temperature >= 20 && temperature <= 30) {
+         return "It’s lovely weather for sports today!";
+      } else if (temperature >= 10 && temperature <= 40) {
+         return "It’s reasonable weather for sports today.";
+      } else {
+         return "Please exercise with care today, watch out for the weather!";
+      }
+   }
+}
---
+++ src/chapter06/exercises/ex6_28/readme.md
@@ -0,0 +1,4 @@
+Write a method **_sportsRecommender_** that inputs a Celsius temperature and returns _**“It’s
+lovely weather for sports today!”**_ if it’s 20–30 °C, _**“It’s reasonable weather for sports today.”**_ if it’s
+10–40 °C, and _**“Please exercise with care today, watch out for the weather!”**_ otherwise. Create an
+application to test the method.
