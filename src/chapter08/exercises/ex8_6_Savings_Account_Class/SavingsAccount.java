e2646f2638820e14ca700bc4880f7832b5ff6645
David Castellar Martínez <castellardav@gmail.com>
2022-07-13 14:17:44 -0500

    reset commits to avoid pyxl account

---
+++ src/chapter08/exercises/ex8_6_Savings_Account_Class/SavingsAccount.java
@@ -0,0 +1,25 @@
+package chapter08.exercises.ex8_6_Savings_Account_Class;
+
+public class SavingsAccount {
+    private static double annualInterestRate;
+    private double savingsBalance;
+
+    public SavingsAccount(double savingsBalance) {
+        this.savingsBalance = savingsBalance / 100;
+    }
+
+    public double getSavingsBalance() {
+        return savingsBalance;
+    }
+
+    public double calculateMonthlyInterest() {
+        double monthlyInterest = savingsBalance * annualInterestRate / 12;
+        savingsBalance += monthlyInterest;
+
+        return monthlyInterest;
+    }
+
+    public static void modifyInterestRate(double annualInterestRate) {
+        SavingsAccount.annualInterestRate = annualInterestRate;
+    }
+}
