93fa46e92cfc8b4a60eed1b7c3f720f3620ac12d
David Castellar Martínez <castellardav@gmail.com>
2022-07-21 16:54:10 -0500

    finishes exercise 09 of chapter 08

--- src/chapter08/exercises/ex8_09_Random_Numbers/RandomNumbers.java
+++ src/chapter08/exercises/ex8_09_Random_Numbers/RandomNumbers.java
@@ -1,14 +1,19 @@
 package chapter08.exercises.ex8_09_Random_Numbers;
 
 import java.security.SecureRandom;
+import java.util.Scanner;
 
 public class RandomNumbers {
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
         SecureRandom random = new SecureRandom();
-        int n = 10;
+        int n;
 
-        for (int i = 0; i < n; i++) {
-            System.out.printf("Random number #%d:", (random.nextInt(100) + 10));
+        System.out.print("Enter the number of randoms you want: ");
+        n = input.nextInt();
+
+        for (int i = 1; i <= n; i++) {
+            System.out.printf("Random number #%02d: %d%n", i, (random.nextInt(91) + 10));
         }
     }
 }
