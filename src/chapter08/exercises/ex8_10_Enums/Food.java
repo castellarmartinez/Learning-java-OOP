4d889f88a33659bd4da009219c21f4be6a81a129
David Castellar Martínez <castellardav@gmail.com>
2022-07-21 17:12:09 -0500

    completes Enums exercise

---
+++ src/chapter08/exercises/ex8_10_Enums/Food.java
@@ -0,0 +1,23 @@
+package chapter08.exercises.ex8_10_Enums;
+
+public enum Food {
+   APPLE("Fruit", 130),
+   BANANA("Fruit", 200),
+   CARROT("Vegetable", 50);
+
+   private final String type;
+   private final int numberOfCalories;
+
+   Food(String type, int numberOfCalories) {
+      this.type = type;
+      this.numberOfCalories = numberOfCalories;
+   }
+
+   public String getType() {
+      return type;
+   }
+
+   public int getNumberOfCalories() {
+      return numberOfCalories;
+   }
+}
---
+++ src/chapter08/exercises/ex8_10_Enums/readme.md
@@ -0,0 +1,3 @@
+Write an **enum** type **Food**, whose constants (**APPLE**, **BANANA**, **CARROT**) take two parameters
+—the type (vegetable or fruit), and number of calories. Write a program to test the **Food enum**
+so that it displays the **enum** names and their information.
