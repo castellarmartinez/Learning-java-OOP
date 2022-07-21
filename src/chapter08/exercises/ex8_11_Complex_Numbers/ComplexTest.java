a22effb5572b4d63836eef4d473c6b2677adeae1
David Castellar Martínez <castellardav@gmail.com>
2022-07-21 17:49:06 -0500

    creates complex class

---
+++ src/chapter08/exercises/ex8_11_Complex_Numbers/ComplexTest.java
@@ -0,0 +1,18 @@
+package chapter08.exercises.ex8_11_Complex_Numbers;
+
+public class ComplexTest {
+   public static void main(String[] args) {
+      Complex complex1 = new Complex(7, 8);
+      Complex complex2 = new Complex(-2, 3);
+
+      System.out.println("First complex number:");
+      complex1.print();
+      System.out.println("Second complex number:");
+      complex2.print();
+
+      System.out.println("\nSum of the two complex number:");
+      Complex.add(complex1, complex2).print();
+      System.out.println("Difference of the two complex number:");
+      Complex.subtract(complex1, complex2).print();
+   }
+}
