8f0d01de11542865bb3413e5bbe706a30b88a631
David Castellar Martinez <castellardav@gmail.com>
2023-06-21 16:25:03 -0500

    rename variable BMI to bmi

--- src/chapter02/exercises/ex2_33_Body_Mass_Index_Calculator/BodyMassIndexCalculator.java
+++ src/chapter02/exercises/ex2_33_Body_Mass_Index_Calculator/BodyMassIndexCalculator.java
@@ -7,16 +7,16 @@ public class BodyMassIndexCalculator {
       Scanner input = new Scanner(System.in);
       int weight;
       int height;
-      int BMI;
+      int bmi;
 
       System.out.print("Enter the weight in kilograms: ");
       weight = input.nextInt();
       System.out.print("Enter the height in centimeters: ");
       height = input.nextInt();
 
-      BMI = weight * 10_000 / (height * height);
-      System.out.printf("The body mass index is %d%n", BMI);
-      System.out.println("\nUnderweight = <18.5\nNormal weight = 18.5–24.9\n" +
-              "Overweight = 25–29.9\nObesity = BMI of 30 or greater");
+      bmi = weight * 10_000 / (height * height);
+      System.out.printf("The body mass index is %d%n", bmi);
+      System.out.println("\nUnderweight = < 18.5\nNormal weight = 18.5–24.9\n" +
+              "Overweight = 25–29.9\nObesity = bmi of 30 or greater");
    }
 }
