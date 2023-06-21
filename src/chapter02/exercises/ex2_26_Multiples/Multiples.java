2f346d0fcd2e474e845ff776a43aa2617cddb8c0
David Castellar Martinez <castellardav@gmail.com>
2023-06-21 16:07:11 -0500

    fix exercise 26 from chapter 2

--- src/chapter02/exercises/ex2_26_Multiples/Multiples.java
+++ src/chapter02/exercises/ex2_26_Multiples/Multiples.java
@@ -9,18 +9,18 @@ public class Multiples {
       int number2;
 
       System.out.println("Enter two integers and I will tell you if" +
-              " the first is a multiple of the second.");
+              " he first number tripled is a multiple of the second number doubled.");
       System.out.print("Your first integer: ");
       number1 = input.nextInt();
       System.out.print("Your second integer: ");
       number2 = input.nextInt();
 
-      if (number1 % number2 == 0) {
-         System.out.println("The first number is a multiple of the second.");
+      if ((number1 * 3) % (number2 * 2) == 0) {
+         System.out.println("The first number tripled is a multiple of the second number doubled.");
       }
 
-      if (number1 % number2 != 0) {
-         System.out.println("The first number is not a multiple of the second.");
+      if ((number1 * 3) % (number2 * 2) != 0) {
+         System.out.println("The first number tripled is not a multiple of the second number doubled.");
       }
    }
 }
