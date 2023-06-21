a4e83af64fb3520fd6be08f44d1578a4a21b952a
David Castellar Martinez <castellardav@gmail.com>
2023-06-21 16:11:10 -0500

    replace repeated string

--- src/chapter02/exercises/ex2_27_Checkerboard_Pattern_Of_Asterisks/CheckerboardPatternOfAsterisks.java
+++ src/chapter02/exercises/ex2_27_Checkerboard_Pattern_Of_Asterisks/CheckerboardPatternOfAsterisks.java
@@ -1,14 +1,17 @@
 package chapter02.exercises.ex2_27_Checkerboard_Pattern_Of_Asterisks;
 
 public class CheckerboardPatternOfAsterisks {
+
    public static void main(String[] args) {
-      System.out.printf("%-16s%n", "* * * * * * * *");
-      System.out.printf("%16s%n", "* * * * * * * *");
-      System.out.printf("%-16s%n", "* * * * * * * *");
-      System.out.printf("%16s%n", "* * * * * * * *");
-      System.out.printf("%-16s%n", "* * * * * * * *");
-      System.out.printf("%16s%n", "* * * * * * * *");
-      System.out.printf("%-16s%n", "* * * * * * * *");
-      System.out.printf("%16s%n", "* * * * * * * *");
+      String filePattern = "* * * * * * * *";
+
+      System.out.printf("%-16s%n", filePattern);
+      System.out.printf("%16s%n", filePattern);
+      System.out.printf("%-16s%n", filePattern);
+      System.out.printf("%16s%n", filePattern);
+      System.out.printf("%-16s%n", filePattern);
+      System.out.printf("%16s%n", filePattern);
+      System.out.printf("%-16s%n", filePattern);
+      System.out.printf("%16s%n", filePattern);
    }
 }
