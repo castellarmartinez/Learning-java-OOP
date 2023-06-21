53f4b88ad778504a7a208a02dc6acfc3fff58ee8
David Castellar Martinez <castellardav@gmail.com>
2023-06-21 16:16:01 -0500

    reformat printf string arguments

--- src/chapter02/exercises/ex2_29_Integer_Value_Of_A_Character/IntegerValueOfACharacter.java
+++ src/chapter02/exercises/ex2_29_Integer_Value_Of_A_Character/IntegerValueOfACharacter.java
@@ -2,19 +2,21 @@ package chapter02.exercises.ex2_29_Integer_Value_Of_A_Character;
 
 public class IntegerValueOfACharacter {
    public static void main(String[] args) {
-      System.out.printf("The character %c has the value %d%n", 'A', ((int) 'A'));
-      System.out.printf("The character %c has the value %d%n", 'B', ((int) 'B'));
-      System.out.printf("The character %c has the value %d%n", 'C', ((int) 'C'));
-      System.out.printf("The character %c has the value %d%n", 'a', ((int) 'a'));
-      System.out.printf("The character %c has the value %d%n", 'b', ((int) 'b'));
-      System.out.printf("The character %c has the value %d%n", 'c', ((int) 'c'));
-      System.out.printf("The character %c has the value %d%n", '0', ((int) '0'));
-      System.out.printf("The character %c has the value %d%n", '1', ((int) '1'));
-      System.out.printf("The character %c has the value %d%n", '2', ((int) '2'));
-      System.out.printf("The character %c has the value %d%n", '$', ((int) '$'));
-      System.out.printf("The character %c has the value %d%n", '*', ((int) '*'));
-      System.out.printf("The character %c has the value %d%n", '+', ((int) '+'));
-      System.out.printf("The character %c has the value %d%n", '/', ((int) '/'));
-      System.out.printf("The character %c has the value %d%n", ' ', ((int) ' '));
+      String stringArgument = "The character %c has the value %d%n";
+
+      System.out.printf(stringArgument, 'A', ((int) 'A'));
+      System.out.printf(stringArgument, 'B', ((int) 'B'));
+      System.out.printf(stringArgument, 'C', ((int) 'C'));
+      System.out.printf(stringArgument, 'a', ((int) 'a'));
+      System.out.printf(stringArgument, 'b', ((int) 'b'));
+      System.out.printf(stringArgument, 'c', ((int) 'c'));
+      System.out.printf(stringArgument, '0', ((int) '0'));
+      System.out.printf(stringArgument, '1', ((int) '1'));
+      System.out.printf(stringArgument, '2', ((int) '2'));
+      System.out.printf(stringArgument, '$', ((int) '$'));
+      System.out.printf(stringArgument, '*', ((int) '*'));
+      System.out.printf(stringArgument, '+', ((int) '+'));
+      System.out.printf(stringArgument, '/', ((int) '/'));
+      System.out.printf(stringArgument, ' ', ((int) ' '));
    }
 }
