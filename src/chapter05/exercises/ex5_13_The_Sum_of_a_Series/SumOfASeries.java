7e2b1c9fdad8aed2e89d2cc583b8d9f1eb267be2
David Castellar Martínez <castellardav@gmail.com>
2023-08-10 10:56:10 -0500

    update: display the summation from 1 to 100

--- src/chapter05/exercises/ex5_13_The_Sum_of_a_Series/SumOfASeries.java
+++ src/chapter05/exercises/ex5_13_The_Sum_of_a_Series/SumOfASeries.java
@@ -4,23 +4,38 @@ import java.util.Scanner;
 
 public class SumOfASeries {
 		public static void main(String[] args) {
-				Scanner input = new Scanner(System.in);
+				int range = 100;
+
+				System.out.println("\tn\tsummation");
+
+				for (int i = 1; i <= range; i++) {
+						displaySummation(i);
+				}
+
+				System.out.println("  n  accumulated product");
+
+				for (int i = 1; i <= range; i++) {
+						displayAccumulatedProduct(i);
+				}
+		}
+
+		private static void displaySummation(int n) {
 				long summation = 0;
+
+				for (int i = 1; i <= n; i++) {
+						summation += i;
+				}
+
+				System.out.println("\t" + n + "\t" + summation);
+		}
+
+		private static void displayAccumulatedProduct(int n) {
 				long productSequence = 1;
 
-				System.out.print("Enter a number between 1-100 and I'll calculate " +
-												"the summation from 1-n: ");
-
-				final int number = input.nextInt();
-
-				for (int i = 1; i <= number; i++) {
-						summation += i;
+				for (int i = 1; i <= n; i++) {
 						productSequence *= i;
 				}
 
-				System.out.println("The summation from 1 to " + number + " is: " +
-												summation);
-				System.out.println("The product sequence from 1 to " + number + " is: "
-												+ productSequence);
+				System.out.println("\t" + n + "\t" + productSequence);
 		}
 }
