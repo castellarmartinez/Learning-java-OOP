17548c4c2cc9aff91f95f110f7299b1896b52642
David Castellar Martínez <castellardav@gmail.com>
2023-08-14 11:44:48 -0500

    refactor: change reasignation of 'found' variable

--- src/chapter05/exercises/ex5_20_Calculating_the_value_of_PI/ValueOfPi.java
+++ src/chapter05/exercises/ex5_20_Calculating_the_value_of_PI/ValueOfPi.java
@@ -11,6 +11,7 @@ public class ValueOfPi {
 				System.out.printf("%8s%12s%n%n", "Terms", "Value of pi");
 
 				for (int i = 1; i <= terms; i++) {
+						// pi = 4/1 - 4/3 + 4/5 - 4/7 + 4/9 - ...
 						pi += (sign * 4.0 / (i + (i - 1)));
 						sign *= -1;
 
@@ -19,14 +20,14 @@ public class ValueOfPi {
 								boolean hasTheSameBeginning = (int) (Math.abs(pi) * 100000) == 314159;
 
 								if (hasTheSameBeginning) {
-										found = !found;
+										found = true;
 								}
 						}
 
 						System.out.printf("%8d%12.8f%n", i, pi);
 				}
 
-				System.out.println("There were need " + count + " to get a value" +
+				System.out.println("There were needed " + count + " to get a value" +
 												" that begins with 3.14159\n");
 		}
 }
