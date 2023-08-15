04edb6e1503d7d89d8d8eec2017bb9ab50377d3a
David Castellar Martínez <castellardav@gmail.com>
2023-08-15 16:36:43 -0500

    fix: change equality operator to .equals to compare strings

--- src/chapter05/exercises/ex5_30_Modified_Autopolicy_Class/AutoPolicyModified.java
+++ src/chapter05/exercises/ex5_30_Modified_Autopolicy_Class/AutoPolicyModified.java
@@ -38,19 +38,10 @@ public class AutoPolicyModified {
 		// sets the state
 		public void setState(String state) {
 
-				if (state == "AL" || state == "AK" || state == "AZ" || state == "AR" ||
-												state == "CA" || state == "CO" || state == "CT" || state == "DE" ||
-												state == "FL" || state == "GA" || state == "HI" || state == "ID" ||
-												state == "IL" || state == "IN" || state == "IA" || state == "KS" ||
-												state == "KY" || state == "LA" || state == "ME" || state == "MD" ||
-												state == "MA" || state == "MI" || state == "MN" || state == "MS" ||
-												state == "MO" || state == "MT" || state == "NE" || state == "NV" ||
-												state == "NH" || state == "NJ" || state == "NM" || state == "NY" ||
-												state == "NC" || state == "ND" || state == "OH" || state == "OK" ||
-												state == "OR" || state == "PA" || state == "RI" || state == "SC" ||
-												state == "SD" || state == "TN" || state == "TX" || state == "UT" ||
-												state == "VT" || state == "VA" || state == "WA" || state == "WV" ||
-												state == "WI" || state == "WY") {
+				if (state.equals("CT") || state.equals("MA")
+												|| state.equals("ME") || state.equals("NH")
+												|| state.equals("NJ") || state.equals("NY")
+												|| state.equals("PA") || state.equals("VT")) {
 						this.state = state;
 				} else {
 						System.out.println("Error in the state code.");
