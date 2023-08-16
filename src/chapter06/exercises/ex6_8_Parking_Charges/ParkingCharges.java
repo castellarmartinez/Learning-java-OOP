93ce5c3c59afcded879070b77fc06db423e18d2f
David Castellar Martínez <castellardav@gmail.com>
2023-08-16 18:43:37 -0500

    refactor: extract inner if

--- src/chapter06/exercises/ex6_8_Parking_Charges/ParkingCharges.java
+++ src/chapter06/exercises/ex6_8_Parking_Charges/ParkingCharges.java
@@ -4,7 +4,7 @@ public class ParkingCharges {
 		public static void main(String[] args) {
 				calculateCharges(2);
 				calculateCharges(6.3);
-				calculateCharges(6.8);
+				calculateCharges(12.8);
 				calculateCharges(21);
 		}
 
@@ -13,10 +13,10 @@ public class ParkingCharges {
 
 				if (hours > 3) {
 						charge += 0.5 * Math.ceil(hours - 3);
+				}
 
-						if (charge > 10) {
-								charge = 10.0;
-						}
+				if (charge > 10) {
+						charge = 10.0;
 				}
 
 				System.out.printf("The total charges for %.1f hours is $%.2f%n",
