e39d2b245997a4e967008d9c9d0121ec5e1b6c84
David Castellar Martínez <castellardav@gmail.com>
2023-08-16 18:50:35 -0500

    fix: delete unnecessary methods

--- src/chapter06/exercises/ex6_9_Rounding_Numbers/RoundingNumbers.java
+++ src/chapter06/exercises/ex6_9_Rounding_Numbers/RoundingNumbers.java
@@ -11,28 +11,10 @@ public class RoundingNumbers {
 
 				System.out.printf("The number entered is %f%n", numberEntered);
 				roundToInteger(numberEntered);
-				roundToTenths(numberEntered);
-				roundToHundredths(numberEntered);
-				roundToThousandths(numberEntered);
 		}
 
 		private static void roundToInteger(double number) {
 				double roundedNumber = Math.floor(number + 0.5);
 				System.out.printf("The nearest integer is %.0f%n", roundedNumber);
-		}
-
-		private static void roundToTenths(double number) {
-				double roundedNumber = Math.floor(number * 10 + 0.5) / 10;
-				System.out.printf("Rounded to tenths %.1f%n", roundedNumber);
-		}
-
-		private static void roundToHundredths(double number) {
-				double roundedNumber = Math.floor(number * 100 + 0.5) / 100;
-				System.out.printf("Rounded to hundredths %.2f%n", roundedNumber);
-		}
-
-		private static void roundToThousandths(double number) {
-				double roundedNumber = Math.floor(number * 1000 + 0.5) / 1000;
-				System.out.printf("Rounded to thousandths %.3f%n", roundedNumber);
 		}
 }
