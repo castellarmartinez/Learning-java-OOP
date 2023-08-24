a4ab236ecb891285328d6341378819140502c749
David Castellar Martínez <castellardav@gmail.com>
2023-08-24 16:27:06 -0500

    feat: finish square and characters exercise

--- src/chapter06/exercises/ex6_19_Displaying_A_Square_Of_Any_Character/SquareOfAnyCharacter.java
+++ src/chapter06/exercises/ex6_19_Displaying_A_Square_Of_Any_Character/SquareOfAnyCharacter.java
@@ -1,9 +1,22 @@
 package chapter06.exercises.ex6_19_Displaying_A_Square_Of_Any_Character;
 
-public class SquareOfAnyCharacter {
-		private static char character;
+import java.util.Scanner;
 
-		public static void squareOfAsterisks(int side) {
+public class SquareOfAnyCharacter {
+		public static void main(String[] args) {
+				Scanner input = new Scanner(System.in);
+				int numberOfAsterisks;
+				char fillCharacter;
+
+				System.out.print("Enter a number and I'll draw an square with that number: ");
+				numberOfAsterisks = input.nextInt();
+				System.out.print("Now enter the character the square will be composed: ");
+				fillCharacter = input.next().charAt(0);
+
+				squareOfAsterisks(numberOfAsterisks, fillCharacter);
+		}
+
+		public static void squareOfAsterisks(int side, char character) {
 				for (int i = 0; i < side; i++) {
 						for (int j = 0; j < side; j++) {
 								System.out.print(character + " ");
@@ -11,9 +24,5 @@ public class SquareOfAnyCharacter {
 
 						System.out.println();
 				}
-		}
-
-		public static void fillCharacter(char characterEntered) {
-				character = characterEntered;
 		}
 }
