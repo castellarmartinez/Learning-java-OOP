1122ed7b074becce8ef405a48358a84ed9e62aba
David Castellar Martínez <castellardav@gmail.com>
2023-09-05 08:15:28 -0500

    feat: finish exercise 6.24 Perfect Numbers

--- src/chapter06/exercises/ex6_24_Perfect_Numbers/PerfectNumbersTest.java
+++ src/chapter06/exercises/ex6_24_Perfect_Numbers/PerfectNumbersTest.java
@@ -5,9 +5,33 @@ public class PerfectNumbersTest {
 				System.out.println("Perfect numbers: ");
 
 				for (int i = 1; i <= 1000000000; i++) {
-						if (PerfectNumbers.isPerfect(i)) {
-								PerfectNumbers.factors(i);
+						if (isPerfect(i)) {
+								factors(i);
 						}
 				}
+		}
+
+		public static boolean isPerfect(int number) {
+				int sum = 0;
+
+				for (int i = 1; i <= number / 2; i++) {
+						if (number % i == 0) {
+								sum += i;
+						}
+				}
+
+				return (sum == number);
+		}
+
+		public static void factors(int number) {
+				System.out.printf("%d: ", number);
+
+				for (int i = 1; i <= number / 2; i++) {
+						if (number % i == 0) {
+								System.out.printf("%d ", i);
+						}
+				}
+
+				System.out.println();
 		}
 }
