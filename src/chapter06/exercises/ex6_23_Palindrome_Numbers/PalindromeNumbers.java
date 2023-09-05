f253ae28a255661c3ce79f2127b10b224be2ccd6
David Castellar Martínez <castellardav@gmail.com>
2023-09-05 08:09:54 -0500

    feat: finish exercise 6.23 Palindrome Numbers

--- src/chapter06/exercises/ex6_23_Palindrome_Numbers/PalindromeNumbers.java
+++ src/chapter06/exercises/ex6_23_Palindrome_Numbers/PalindromeNumbers.java
@@ -1,8 +1,20 @@
 package chapter06.exercises.ex6_23_Palindrome_Numbers;
 
+import java.util.Scanner;
+
 public class PalindromeNumbers {
-		public static boolean isPalindrome(int number, int digits) {
+		public static void main(String[] args) {
+				Scanner input = new Scanner(System.in);
+
+				System.out.println("Enter a five-digit number and I'll tell you if it's palindrome: ");
+				int palindrome = input.nextInt();
+				System.out.printf("The number entered %s a palindrome number.",
+												(isPalindrome(palindrome) ? "is" : "is not"));
+		}
+
+		public static boolean isPalindrome(int number) {
 				boolean isAPalindromeNumber = true;
+				int digits = getNumberOfDigits(number);
 
 				for (int i = 0; i < digits / 2; i++) {
 
@@ -19,5 +31,17 @@ public class PalindromeNumbers {
 				}
 
 				return isAPalindromeNumber;
+		}
+
+		private static int getNumberOfDigits(int number) {
+				int i = 1;
+				int numberOfDigits = 0;
+
+				while (number / i > 0) {
+						i *= 10;
+						numberOfDigits++;
+				}
+
+				return numberOfDigits;
 		}
 }
