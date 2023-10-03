e92215e28c1bf5a1d45dfdf0bed70f24f29008fd
David Castellar Martínez <castellardav@gmail.com>
2023-10-03 11:30:24 -0500

    feat: implement random moving in case of tie for accessibility squares

--- src/chapter07/exercises/ex7_22_Knights_Tour/KnightsTourHeuristic.java
+++ src/chapter07/exercises/ex7_22_Knights_Tour/KnightsTourHeuristic.java
@@ -1,11 +1,13 @@
 package chapter07.exercises.ex7_22_Knights_Tour;
 
+import java.security.SecureRandom;
 import java.util.Scanner;
 
 public class KnightsTourHeuristic {
 		static int currentRow = 0;
 		static int currentColumn = 0;
 		static int numberOfMovements = 0;
+		static SecureRandom random = new SecureRandom();
 		static final int[] horizontal = {2, 1, -1, -2, -2, -1, 1, 2};
 		static final int[] vertical = {-1, -2, -2, -1, 1, 2, 2, 1};
 		static final char[][] board = new char[8][8];
@@ -36,7 +38,7 @@ public class KnightsTourHeuristic {
 				while (numberOfMovements < 63) {
 						int moveNumber = obtainBestMove();
 
-						if (moveNumber == 8) {
+						if (moveNumber == -1) {
 								break;
 						}
 
@@ -47,20 +49,36 @@ public class KnightsTourHeuristic {
 				System.out.printf("%nProgram run %d times%n.", numberOfMovements);
 		}
 
+		static private void initializeBoard() {
+				for (int i = 0; i < 8; i++) {
+						for (int j = 0; j < 8; j++) {
+								board[i][j] = '0';
+						}
+				}
+		}
+
 		private static int obtainBestMove() {
-				int minimumScore = 9;
-				int bestMove = 8;
+				int minimumScore = Integer.MAX_VALUE;
+				int bestMove = -1;
 
 				for (int i = 0; i < 8; i++) {
 						int newRowPosition = currentRow + vertical[i];
 						int newColumnPosition = currentColumn + horizontal[i];
 
-						if (!isInsideBoard(newRowPosition, newColumnPosition) ||
+						if (isOutsideBoard(newRowPosition, newColumnPosition) ||
 														isPositionTaken(newRowPosition, newColumnPosition)) {
 								continue;
 						}
 
 						int movementScore = accessibility[newRowPosition][newColumnPosition];
+
+						if (movementScore == minimumScore) {
+								int faceValue = random.nextInt(2);
+
+								if (faceValue == 1) {
+										bestMove = i;
+								}
+						}
 
 						if (movementScore < minimumScore) {
 								minimumScore = movementScore;
@@ -71,24 +89,16 @@ public class KnightsTourHeuristic {
 				return bestMove;
 		}
 
-		static private void initializeBoard() {
-				for (int i = 0; i < 8; i++) {
-						for (int j = 0; j < 8; j++) {
-								board[i][j] = '0';
-						}
-				}
-		}
-
-		private static boolean isInsideBoard(int rowPosition, int columnPosition) {
+		private static boolean isOutsideBoard(int rowPosition, int columnPosition) {
 				if (rowPosition < 0 || rowPosition > 7) {
-						return false;
+						return true;
 				}
 
 				if (columnPosition < 0 || columnPosition > 7) {
-						return false;
+						return true;
 				}
 
-				return true;
+				return false;
 		}
 
 		private static boolean isPositionTaken(int rowPosition, int columnPosition) {
